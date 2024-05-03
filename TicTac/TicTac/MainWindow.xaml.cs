fee3570e1393340ce3ca0982a44c0fc4e3d37df3
unknown <jacek.druc@infotech.edu.pl>
2024-05-03 16:23:56 +0200

    import c# projects

---
+++ TicTac/TicTac/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        private bool playerTurn = true; // true - Gracz X, false - Gracz O
+        private object grid;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (button.Content == "")
+            {
+                if (playerTurn)
+                    button.Content = "X";
+                else
+                    button.Content = "O";
+
+                playerTurn = !playerTurn;
+
+                if (CheckForWinner())
+                {
+                    MessageBox.Show((playerTurn ? "O" : "X") + " wygrywa!", "Koniec gry");
+                    ResetGame();
+                }
+                else if (IsBoardFull())
+                {
+                    MessageBox.Show("Remis!", "Koniec gry");
+                    ResetGame();
+                }
+            }
+        }
+
+        private bool CheckForWinner()
+        {
+         
+            for (int i = 0; i < 3; i++)
+            {
+                if (CheckRowCol(btn1.Content, btn2.Content, btn3.Content) ||
+                    CheckRowCol(btn4.Content, btn5.Content, btn6.Content) ||
+                    CheckRowCol(btn7.Content, btn8.Content, btn9.Content))
+                {
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (CheckRowCol(btn1.Content, btn4.Content, btn7.Content) ||
+                    CheckRowCol(btn2.Content, btn5.Content, btn8.Content) ||
+                    CheckRowCol(btn3.Content, btn6.Content, btn9.Content))
+                {
+                    return true;
+                }
+            }
+
+            if (CheckRowCol(btn1.Content, btn5.Content, btn9.Content) ||
+                CheckRowCol(btn3.Content, btn5.Content, btn7.Content))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckRowCol(object b1, object b2, object b3)
+        {
+            return (!string.IsNullOrEmpty(b1.ToString()) && b1 == b2 && b2 == b3);
+        }
+
+        private bool IsBoardFull()
+        {
+            foreach (var item in grid.Children)
+            {
+                if (item is Button button && string.IsNullOrEmpty(button.Content.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ResetGame()
+        {
+            foreach (var item in grid.Children)
+            {
+                if (item is Button button)
+                {
+                    button.Content = "";
+                }
+            }
+
+            playerTurn = true;
+        }
+    }
+}
---
+++ pocztowkaE/pocztowkaE/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace pocztowkaE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+        }
+        
+
+
+
+        private void buttonSprCen_Click(object sender, RoutedEventArgs e)
+        {
+            if (radio1po.IsChecked == true)
+            {
+                cena.Content = "Cena: 1zł";
+                imagepoczt.Visibility = Visibility.Visible;
+                imagelist.Visibility = Visibility.Hidden;
+                imagepacz.Visibility = Visibility.Hidden;
+
+            } else if (radio2li.IsChecked == true)
+            {
+                cena.Content = "Cena: 1,5zł";
+                imagelist.Visibility = Visibility.Visible;
+                imagepacz.Visibility = Visibility.Hidden;
+                imagepoczt.Visibility = Visibility.Hidden;
+
+
+            }
+            else if (radio3pa.IsChecked == true)
+            {
+                cena.Content = "Cena: 10zł";
+                imagepacz.Visibility = Visibility.Visible;
+                imagepoczt.Visibility = Visibility.Hidden;
+                imagelist.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void buttonZatw_Click(object sender, RoutedEventArgs e)
+        {
+            int ilosc = kod.Text.Length;
+
+
+            if (ilosc > 5 && ilosc < 5)
+            {
+
+
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+
+            }
+            else if (kod.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("Dane przesyłki zostały wprowadzone");
+            }
+            else
+            { 
+                MessageBox.Show("Kod pocztowy powinien się składać z samych cyfr");
+           
+            }
+           
+        }
+
+
+    }
+}
